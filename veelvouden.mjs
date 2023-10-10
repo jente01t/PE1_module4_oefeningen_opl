c2400405dd63d8f8493aeb805c75c3e4536e52ca
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-10 09:40:43 +0200

    Update veelvouden.mjs

--- veelvouden.mjs
+++ veelvouden.mjs
@@ -7,4 +7,9 @@ let end = parseFloat(await userInput.question('Met welk getal eindigen we?'));
 let deler1 = parseFloat(await userInput.question('Wat is het eerste getal waarop we testen?'));
 let deler2 = parseFloat(await userInput.question('Wat is het tweede getal waarop we testen?'));
 
+for(let i = start; i <= end; i++) {
+    if ((i % deler1 == 0) && (i % deler2 == 0));
+    console.log(i)
+}
 
+process.exit();
