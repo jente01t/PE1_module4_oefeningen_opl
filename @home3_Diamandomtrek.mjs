a91d2cacc194b403f386f586372f0b18f7a51475
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-10 18:02:37 +0200

    update main

--- @home3_Diamandomtrek.mjs
+++ @home3_Diamandomtrek.mjs
@@ -8,7 +8,19 @@ let midden = (hoogte + 1) / 2
 
 //bovenste deel
 
-for (let i = 1; i)
+for (let i = 1; i <= midden; i++) {
+    let rij = '';
+    for (let j = 1; j <= midden - i; j++) {
+        rij += ' ';
+    } rij += '*'
+    for (let k = 1; k < i; k++) {
+        rij += ' ';
+    } 
+    if (i > 1) {
+        rij += '*'
+    }
+    console.log(rij)
+}
 
 
 
