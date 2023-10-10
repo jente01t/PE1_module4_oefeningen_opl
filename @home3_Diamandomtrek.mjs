e492b31fac7dc2a31be2fb6f45a1f8d3358f555c
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-10 17:12:30 +0200

    Create @home3_Diamandomtrek.mjs

---
+++ @home3_Diamandomtrek.mjs
@@ -0,0 +1,24 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
+
+let hoogte = parseFloat(await userInput.question('Geef de hoogte van de diamand: '));
+let midden = (hoogte + 1) / 2
+
+
+//bovenste deel
+
+for (let i = 1; i)
+
+
+
+
+
+
+//onderste deel
+
+
+
+
+
+process.exit();
